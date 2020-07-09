21a7e20f367edeac6c3671bee555ca3cdac79edc
Selim Yalınkılıç <yalinkilicselim@gmail.com>
2020-07-09 22:49:25 +0300

    yagizdoweb updated app.js and added fade animation using framer motion

--- src/components/App.js
+++ src/components/App.js
@@ -15,15 +15,24 @@ import Projects from './Projects/Projects';
 import Footer from './Footer/Footer';
 import Blog from './Blog/Blog';
 import ScrollTop from './ScrollTop/ScrollTop';
+import { motion } from 'framer-motion';
 
 const App = () => {
   const [theme, toggleTheme] = useDarkMode();
   const themeMode = theme === 'light' ? lightTheme : darkTheme;
   const themeClass = theme === 'light' ? "lightContent" : "darkContent";
+  const variants = {
+    hidden: { opacity: 0},
+    show: { 
+            opacity: 1,
+            transition: {
+              duration: 1
+            }
+    }};
   return (
     <ThemeProvider theme={themeMode}>
       <GlobalStyles />
-      <div className={themeClass} id="main">
+      <motion.div className={themeClass} id="main" initial="hidden" animate="show" variants={variants}>
         <Container>
           <Menu theme={theme} toggleTheme={toggleTheme} />
           <section>
@@ -35,7 +44,7 @@ const App = () => {
         </Container>
         <Footer/>
         <ScrollTop />
-      </div>
+      </motion.div>
     </ThemeProvider> 
   )
 }
